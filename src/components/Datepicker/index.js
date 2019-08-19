fef2c2775c6a5f27c512351c4c2a8398240820c5
Girish Rathod <rathodgirishk@gmail.com>
2019-08-19 16:27:10 +0530

    Create separate component for datepicker

---
+++ src/components/Datepicker/index.js
@@ -0,0 +1,19 @@
+
+
+import React from "react";
+
+class Datepicker extends React.Component {
+    componentDidMount() {}
+
+    componentDidUpdate(prevProps) {}
+
+    render() {
+        return (
+            <h6>Datepicker component will display here</h6>
+        );
+    }
+
+}
+
+export default Datepicker;
+
