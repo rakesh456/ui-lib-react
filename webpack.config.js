2d507e6c55550618daa716e05fef2c4f771b0c02
Girish Rathod <rathodgirishk@gmail.com>
2019-08-27 12:33:09 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.less$/,
+                test: /\.css$/,
                 use: [
                     'style-loader',
                     'css-loader',
