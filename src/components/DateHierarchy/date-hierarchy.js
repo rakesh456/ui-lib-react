231a6044fc24ff1f32e5dd36baab48a24e0294db
Girish Rathod <rathodgirishk@gmail.com>
2019-11-21 10:13:13 +0530

    Added date hierarchy component initial files

---
+++ src/components/DateHierarchy/date-hierarchy.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class DateHierarchy extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+
+    updateDimensions() {}
+
+    componentDidMount() {
+    }
+
+    render() {
+
+        return (
+            <div className="VS-App">
+                Test
+            </div>
+        );
+    }
+}
+
+export default DateHierarchy;
