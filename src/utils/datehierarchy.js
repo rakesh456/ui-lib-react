858e2b83d919a80cd62846a92b937d006e97e86c
dobal19 <dobalsachin97@gmail.com>
2019-12-18 15:42:49 +0530

    added some feature to date hierarchy

--- src/utils/datehierarchy.js
+++ src/utils/datehierarchy.js
@@ -17,7 +17,6 @@ export const getMonthDays = (month, year) => {
           }
           return days;
 }
-
 export const getChildren = function (year) {
   
   var quarterArray = [{
@@ -158,4 +157,181 @@ if(lowerLimit > 999 && upperLimit>999 && (lowerLimit <= upperLimit) && lowerLimi
    
     
   }
+}
+
+
+
+export const getListOfYearsForWeeks = function(lowerLimit, upperLimit) {
+  if(lowerLimit > 999 && upperLimit>999 && (lowerLimit <= upperLimit) && lowerLimit % 1 ==0 && upperLimit % 1 == 0)
+  {
+    console.log("lowerLimit",lowerLimit);
+      let years = [];
+      while (lowerLimit <= upperLimit) {
+       var year = {
+        "year":parseInt(lowerLimit),
+        "showChild": false,
+        "state": 0,
+        "children": getChildrenForWeeks(lowerLimit)
+       }
+       years.push(year);
+       lowerLimit++;
+      }
+      return years;
+     }
+    
+    else
+    {
+      lowerLimit = 2;
+      upperLimit = 1;
+      let years = [];
+      while (lowerLimit <= upperLimit) {
+       var year = {
+        "year": lowerLimit,
+        "showChild": false,
+        "state": 0,
+        "children": getChildrenForWeeks(lowerLimit)
+       }
+       years.push(year);
+       lowerLimit++;
+      }
+      console.error("either lowerLimit or upperLimit is not in valid format");
+      return years;
+     
+      
+    }
+  }
+
+
+  export const getChildrenForWeeks = function (year) {
+  
+    var quarterArray = [{
+    "quarter": "Q1",
+    "showChild": false,
+    "state": 0,
+    "children": [{
+      "month": "Jan",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(1, year)
+     },
+     {
+      "month": "Feb",
+      "showChild": false,
+      "state": 0,
+      "children":getMonthWeeks(2, year),
+  
+     },
+     {
+      "month": "Mar",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(3, year)
+     }
+    ]
+   },
+   {
+    "quarter": "Q2",
+    "showChild": false,
+    "state": 0,
+    "children": [{
+      "month": "Apr",
+      "showChild": false,
+      "state": 0,
+      "children":getMonthWeeks(4, year)
+     },
+     {
+      "month": "May",
+      "showChild": false,
+      "state": false,
+      "children":getMonthWeeks(5, year)
+     },
+     {
+      "month": "Jun",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(6, year)
+     }
+    ]
+   },
+   {
+    "quarter": "Q3",
+    "showChild": false,
+    "state": 0,
+    "children": [{
+      "month": "Jul",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(7, year)
+     },
+     {
+      "month": "Aug",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(8, year)
+     },
+     {
+      "month": "Sep",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(9, year)
+     }
+    ]
+   },
+   {
+    "quarter": "Q4",
+    "showChild": false,
+    "state": 0,
+    "children": [{
+      "month": "Oct",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(10, year)
+     },
+     {
+      "month": "Nov",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(11, year)
+     },
+     {
+      "month": "Dec",
+      "showChild": false,
+      "state": 0,
+      "children": getMonthWeeks(12, year)
+     }
+    ]
+   }
+  ]
+  return quarterArray;
+  }
+
+  export const getMonthWeeks = function (month_number,year ) {
+
+    var firstOfMonth = new Date(year, month_number-1, 1);
+    var lastOfMonth = new Date(year, month_number, 0);
+    var used = firstOfMonth.getDay() + lastOfMonth.getDate();
+    var weeks = [];
+    var start = 1;
+    var weekdays = new Array(7);
+    weekdays[0] = "Sun";
+    weekdays[1] = "Mon";
+    weekdays[2] = "Tue";
+    weekdays[3] = "Wed";
+    weekdays[4] = "Thu";
+    weekdays[5] = "Fri";
+    weekdays[6] = "Sat";
+    for(let weekNo = 1; weekNo <=  Math.ceil(used/7); weekNo++){
+      var weekobj = { week : "week "+ weekNo, state: 0, showChild: false, children:[]}
+      for(var i = start; i < (lastOfMonth).getDate()+1;i++ ){
+          var monthDate = new Date(year, month_number-1, i);
+          var dayObj = {date: i, day: weekdays[monthDate.getDay()], state: false}; 
+          weekobj.children.push(dayObj);
+          if(monthDate.getDay()===6){
+              start = i + 1;
+              break;
+          }
+      }
+      weeks.push(weekobj);
+    }
+    return weeks;
 }
