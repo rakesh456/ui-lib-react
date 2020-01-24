789cd5818df887fb14e5e213720d136ccae4c261
dobal19 <dobalsachin97@gmail.com>
2020-01-24 11:13:45 +0530

    resoved some small issues

--- src/components/DateHierarchy/datehierarchyView.js
+++ src/components/DateHierarchy/datehierarchyView.js
@@ -545,6 +545,7 @@ class DatehierarchyView extends React.PureComponent {
     }
 
     updateSelectAllCheckboxHandler = () => {
+        console.log('onsearchallcheckbox');
         let _isSelectAll = true;
         let { years } = this.state;
         let selectAllState = 0;
@@ -555,7 +556,7 @@ class DatehierarchyView extends React.PureComponent {
                 selectAllState = selectAllState + yr.state;
             }
         });
-        // console.log(selectAllState, ' _isSelectAll ', _isSelectAll);
+        console.log(selectAllState, ' _isSelectAll ', _isSelectAll);
 
         this.setState({
             isSelectAll: _isSelectAll,
