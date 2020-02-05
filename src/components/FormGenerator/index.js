377becf73732886196c6d1bc2cdb6b7795b87c84
rakesh456 <rakesh.formal@gmail.com>
2020-02-05 14:23:07 +0530

    initial commit

---
+++ src/components/FormGenerator/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class FormGenerator extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        const datePickerOptions = this.props.options;
+        this.state = {};
+    }
+
+    componentDidMount() {}
+
+    componentDidUpdate() {}
+
+    render() {}
+}
