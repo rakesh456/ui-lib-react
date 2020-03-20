111dd35776ba86605d0f71b322be8d28a301d755
Girish Rathod <rathodgirishk@gmail.com>
2020-03-20 18:13:09 +0530

    Resolved disabled year issue in DP

--- src/components/Datepicker/calendar-other/years-view.js
+++ src/components/Datepicker/calendar-other/years-view.js
@@ -90,15 +90,20 @@ class YearsView extends React.PureComponent {
     
     checkYearIsEnabled = (year) => {
         const {disabledList} = this.props.options;
+
+        const { lowerYearLimit, lowerMonthLimit } = getYYYYForLowerLimit(this.props.options);
+        const { upperYearLimit, upperMonthLimit } = getYYYYForUpperLimit(this.props.options);
+
         const months = [...Array(12).keys()];
         let allMonthDisabled = 0;
         months.forEach((month) => {
             const val = (('0' + (month + 1)).slice(-2)) + '/' + year.toString();
-            if(disabledList.indexOf(val.toString()) !== -1){
+            const disableByLimit = (lowerMonthLimit > month && lowerYearLimit === year)? true : ((upperMonthLimit <= month && upperYearLimit === year)? true : false);
+
+            if(disabledList.indexOf(val.toString()) !== -1 || disableByLimit === true){
                 allMonthDisabled++;
             }
         });
-
         return (allMonthDisabled >= 12)? false : ((disabledList && disabledList.length > 0 && year)? ((disabledList.indexOf(year.toString()) !== -1)? false : true) : true);
     }
 
